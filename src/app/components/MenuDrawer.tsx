18fe8b6515b0a9b3fcb590260852a7f28a58e8b9
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2025-09-08 14:59:59 -0300

    Sistema de menu de gaveta

---
+++ src/app/components/MenuDrawer.tsx
@@ -0,0 +1,48 @@
+import { useMenu } from "@/menu";
+import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+export default function MenuDrawer() {
+	const menu = useMenu();
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        menu.closeMenu();
+      }
+    }
+
+    if (menu.isOpen) {
+      document.addEventListener("click", handleClickOutside);
+      document.addEventListener("mousedown", handleClickOutside);
+
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [menu.isOpen, menu.closeMenu, menu]);
+
+	return (
+		<motion.div
+			ref={menuRef}
+			initial={{ x: "-100%" }}
+			animate={{ x: 0 }}
+			exit={{ x: "-100%" }}
+			transition={{duration: 0.4, ease: "easeInOut"}}
+			className="fixed top-0 left-0 h-full w-2/5 bg-white shadow-2xl z-50 flex flex-col"
+		>
+			<div className="flex items-center justify-between p-4 border-b">
+				<h2 className="text-xl font-semibold">Menu</h2>
+				<button
+					className="cursor-pointer w-8"
+					onClick={menu.closeMenu}
+				  aria-label="Fechar menu"
+				>
+					✕
+				</button>
+			</div>
+		</motion.div>
+	);
+}
--- src/app/components/NavBarGlobal.tsx
+++ src/app/components/NavBarGlobal.tsx
@@ -1,51 +1,54 @@
 "use client";
 
+import { useMenu } from "@/menu";
 import Image from "next/image";
-import { useState } from "react";
+import MenuDrawer from "./MenuDrawer";
+import { AnimatePresence } from "framer-motion";
 
 export default function NavBarGlobal() {
-	const [isMenuOpen, setMenuOpen] = useState(false);
+	const menu = useMenu();
 	
 	return (
-			<div className="flex h-20 p-4 justify-between w-full bg-white">
-				<Image 
-					src="https://i.postimg.cc/50cK06nC/pngtree-paper-icon-png-image-6294297.png"
-					alt="Logo do blog"
-					width={100}
-					height={100}
-					className="h-11 w-auto object-contain"
-				/>
-				<div className="flex items-center gap-4">
-					<button 
-						className="bg-blue-500 cursor-pointer text-white px-5 py-2 rounded-lg hover:bg-blue-600 transition">
-						fazer login
-					</button>
-					 <div
-							onClick={() => setMenuOpen(!isMenuOpen)}
-							className="flex flex-col justify-between w-6 h-5 cursor-pointer relative"
-						>
-							{/* Linha 1 */}
-							<span
-								className={`h-1 w-full bg-neutral-900 rounded-lg transition-transform duration-300 ${
-									isMenuOpen ? "rotate-45 translate-y-2" : ""
-								}`}
-							></span>
-
-							{/* Linha 2 */}
-							<span
-								className={`h-1 w-full bg-neutral-900 rounded-lg transition-all duration-300 ${
-									isMenuOpen ? "opacity-0" : "opacity-100"
-								}`}
-							></span>
-
-							{/* Linha 3 */}
-							<span
-								className={`h-1 w-full bg-neutral-900 rounded-lg transition-transform duration-300 ${
-									isMenuOpen ? "-rotate-45 -translate-y-2" : ""
-								}`}
-							></span>
-						</div>
+			<>
+				<div className="flex h-20 p-4 justify-between w-full bg-white">
+					<Image 
+						src="https://i.postimg.cc/50cK06nC/pngtree-paper-icon-png-image-6294297.png"
+						alt="Logo do blog"
+						width={100}
+						height={100}
+						className="h-11 w-auto object-contain"
+					/>
+					<div className="flex items-center gap-4">
+						<button
+							className="bg-blue-500 cursor-pointer text-white px-5 py-1 rounded-lg hover:bg-blue-700 transition">
+							fazer login
+						</button>
+						<div
+								onClick={menu.toggleMenu}
+								className="flex flex-col justify-between w-6 h-5 cursor-pointer relative"
+							>
+								<span
+									className={`h-1 w-full bg-neutral-900 rounded-lg transition-transform duration-300 ${
+										menu.isOpen ? "rotate-45 translate-y-2" : ""
+									}`}
+								></span>
+								<span
+									className={`h-1 w-full bg-neutral-900 rounded-lg transition-all duration-300 ${
+										menu.isOpen ? "opacity-0" : "opacity-100"
+									}`}
+								></span>
+								<span
+									className={`h-1 w-full bg-neutral-900 rounded-lg transition-transform duration-300 ${
+										menu.isOpen ? "-rotate-45 -translate-y-2" : ""
+									}`}
+								></span>
+							</div>
+					</div>
 				</div>
-			</div>
+				<AnimatePresence>
+					{menu.isOpen && <MenuDrawer />}
+				</AnimatePresence>
+			</>
+			
 	)
 }
