8690a87d903a946d2f3c5183dea12b00b539d4f8
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2025-09-13 09:58:22 -0300

    Mudança de config

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,20 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.postimg.cc',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ui-avatars.com',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'i.postimg.cc', pathname: '/**' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'ui-avatars.com', pathname: '/**' },
     ],
   },
 };
